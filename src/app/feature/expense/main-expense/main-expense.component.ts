572d025ccd5b5349b552bf0729c1f335e9f385d1
Pradip Patoliya <pradip.patoliya@enaviya.com>
2025-03-11 17:33:34 +0530

    dev_11_03

--- src/app/feature/expense/main-expense/main-expense.component.ts
+++ src/app/feature/expense/main-expense/main-expense.component.ts
@@ -1,32 +1,59 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
-import { MatTabNav, MatTabsModule } from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { ExpenseService } from '../service/expense-service.service';
-declare var $: any;
+import { map, Observable, of, startWith, switchMap, take } from 'rxjs';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-main-expense',
   imports: [
     CommonModule,
     MatTabsModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule
   ],
   templateUrl: './main-expense.component.html',
   styleUrl: './main-expense.component.scss'
 })
 export class MainExpenseComponent {
+  @ViewChild('datepickerInput', { static: false }) datepickerInput!: ElementRef;
   travelRequests: any;
+  travelRequestJsonInfo: any;
+  travelModeList: any;
+  travelClassList: any;
+  originControl = new FormControl('');
+  cities = [];
+  filteredCities$: Observable<{ CityMasterId: number; City: string }[]>;
+  travelPaymentList: any;
 
   constructor(
     private expenseService: ExpenseService
-  ){}
+  ) { 
+    this.filteredCities$ = this.originControl.valueChanges.pipe(
+      startWith(''),
+      switchMap(value => (value?.trim() ? this.expenseService.getCityAuto(value) : of([])))
+    );
+  }
 
   ngOnInit() {
     this.fetchPendingTravelRequests();
+    this.fetchTravelModeList();
+    this.fetchTravelPayemntTypeList();
+    this.fetchExpensePolicyEntitlement();
   }
 
   fetchPendingTravelRequests(): void {
-    this.expenseService.getPendingTravelRequests().subscribe({
+    this.expenseService.getPendingTravelRequests().pipe(take(1)).subscribe({
       next: (response) => {
         this.travelRequests = response;
         console.log('Pending Travel Requests:', this.travelRequests);
@@ -36,5 +63,66 @@ export class MainExpenseComponent {
       }
     });
   }
-  
+
+  onSelectTravelExpenseRequest(event: any) {
+    let travelRequestId = Number(event.target.value) || 0;
+    if (travelRequestId) {
+      this.expenseService.getTravelRequestJsonInfo(travelRequestId).pipe(take(1)).subscribe({
+        next: (response) => {
+          this.travelRequestJsonInfo = response;
+        },
+        error: (error) => {
+          console.error('Error fetching travel request json info:', error);
+        }
+      })
+    }
+  }
+
+  fetchTravelModeList() {
+    this.expenseService.getTravelModeList().pipe(take(1)).subscribe({
+      next: (response) => {
+        this.travelModeList = response;
+      },
+      error: (error) => {
+        console.error('Error fetching travel modes:', error);
+      }
+    })
+  }
+
+  onSelectTravelMode(event: any) {
+    let travelModeKey = event.target.value || 0;
+    if(travelModeKey) {
+      this.expenseService.getTravelClassList(travelModeKey).pipe(take(1)).subscribe({
+        next: (response) => {
+          this.travelClassList = response;
+        },
+        error: (error) => {
+          console.error('Error fetching travel modes:', error);
+        }
+      })
+    }
+  }
+
+  fetchTravelPayemntTypeList() {
+    this.expenseService.getTravelPaymentType().pipe(take(1)).subscribe({
+      next: (response) => {
+        this.travelPaymentList = response;
+      },
+      error: (error) => {
+        console.error('Error fetching travel payments:', error);
+      }
+    })
+  }
+
+  fetchExpensePolicyEntitlement() {
+    this.expenseService.getExpensePolicyEntitlement().pipe(take(1)).subscribe({
+      next: (response) => {
+        console.log(response)
+      },
+      error: (error) => {
+        console.error('Error fetching travel payments:', error);
+      }
+    })
+  }
+
 }
--- src/app/feature/expense/service/expense-service.service.ts
+++ src/app/feature/expense/service/expense-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environment';
@@ -9,14 +9,72 @@ import { environment } from '../../../../environment';
 export class ExpenseService {
 
   private baseUrl = environment.apiUrl;
-  private apiUrl = `${this.baseUrl}/Expense/GetTravelRequestsPendingForClaim?UserMasterId=4&TravelTypeId=0&_=1739778373970`;  // API URL
+  private apiTravelRequestsPendingForClaimUrl = `${this.baseUrl}/Expense/GetTravelRequestsPendingForClaim?UserMasterId=4&TravelTypeId=0`;
+  private apiTravelRequestJsonInfo = `${this.baseUrl}/Expense/GetTravelRequestJsonInfo`;
+
+  private apiTravelDDLData = `${this.baseUrl}/Expense/GetTravelDDLData`;
+  private apiCityAuto = `${this.baseUrl}/Master/GetCityAuto`;
+
+  private apiExpensePolicyEntitlement = `${this.baseUrl}/Expense/GetExpensePolicyEntitlement
+`
+
 
   constructor(
     private http: HttpClient
   ) { }
 
-  // Method to get pending travel requests for claim
+  // Pending travel requests for claim
   getPendingTravelRequests(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+    return this.http.get<any>(this.apiTravelRequestsPendingForClaimUrl);
   }
+
+  // Travel Request Json Info
+  getTravelRequestJsonInfo(travelRequestId: number): Observable<any> {
+    const requestBody = {
+      travelRequestId: travelRequestId
+    };
+    // const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>(this.apiTravelRequestJsonInfo, requestBody);
+  }
+
+  // Travel Mode List
+  getTravelModeList(): Observable<any> {
+    let ddlType = 'TravelMode';
+    return this.http.get<any>(`${this.apiTravelDDLData}?ddlType=${ddlType}`);
+  }
+
+  // Travel Class List
+  getTravelClassList(refId: number): Observable<any> {
+    let ddlType = 'TravelClass';
+    return this.http.get<any>(`${this.apiTravelDDLData}?ddlType=${ddlType}&RefId=${refId}`);
+  }
+
+  // City Auto
+  getCityAuto(term: string) {
+    return this.http.get<any>(`${this.apiCityAuto}?term=${term}&typeid=53`);
+  }
+
+  // Payment Type List
+  getTravelPaymentType() {
+    let ddlType = 'PayType';
+    return this.http.get<any>(`${this.apiTravelDDLData}?ddlType=${ddlType}`);
+  }
+
+  // Expense Policy Entitlement
+  getExpensePolicyEntitlement() {
+    const requestBody = {
+      ClaimTypeId: 53,
+      UserMasterId: 4,
+      ExpenseCategoryId: 3,
+      ReferenceDate: '06-Mar-2024',
+      CityGradeId: 8,
+      CountryGradeId: 0,
+      PolicyReferenceId: 53,
+      TravelRequestId: 37
+    };
+    // const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>(this.apiExpensePolicyEntitlement, requestBody);
+  }
+
+
 }
