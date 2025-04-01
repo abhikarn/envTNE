aeff1020f1fc0796c7babefcf780f00903a51cfa
Pradip Patoliya <pradip.patoliya@enaviya.com>
2025-04-01 15:16:54 +0530

    dev-01-04

--- src/app/shared/dynamic-form/form-controls/gst/gst.component.ts
+++ src/app/shared/dynamic-form/form-controls/gst/gst.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatRadioModule } from '@angular/material/radio';
 import { IFormControl } from '../../form-control.interface';
+import { SnackbarService } from '../../../service/snackbar.service';
 
 @Component({
   selector: 'app-gst',
@@ -14,8 +15,9 @@ import { IFormControl } from '../../form-control.interface';
   templateUrl: './gst.component.html'
 })
 export class GstComponent {
-  @Input() control: FormControl = new FormControl('');
+  @Input() control: any;
   @Input() controlConfig: IFormControl = { name: '' };
+  @Input() amountControl: any;
   companyGSTForm: FormGroup;
   gstDetailsForm: FormGroup;
   gstDetails: any = {
@@ -32,12 +34,15 @@ export class GstComponent {
     }
   ]
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private snackbarService: SnackbarService
+  ) {
     this.companyGSTForm = this.fb.group({
       IsBillRaisedInCompanyGST: [false, Validators.required]
     });
     this.gstDetailsForm = this.fb.group({
-      GstIn: ['', Validators.required],
+      GstIn: ['', [Validators.required, this.validateGST]],
       InvoiceNumber: ['', Validators.required],
       Amount: ['', [Validators.required, Validators.min(0)]],
       SGST: ['', Validators.required],
@@ -47,21 +52,66 @@ export class GstComponent {
     });
   }
 
+  ngOnInit() {
+  }
 
   trackByFn(index: number): any {
     return index;
   }
 
+  validateGST(control: FormControl) {
+    const gstRegex = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/;
+    const gstNumber = control.value?.toUpperCase().trim();
+    return gstRegex.test(gstNumber) ? null : { invalidGST: true };
+  }
+
+  validateGSTWithClaimed() {
+    let isValidGst = false;
+    let gstAmount = 0;
+    const gstNum = parseFloat(this.gstDetailsForm.get('Amount')?.value || "0");
+    const sgst = parseFloat(this.gstDetailsForm.get('SGST')?.value || "0");
+    const cgst = parseFloat(this.gstDetailsForm.get('CGST')?.value || "0");
+    const igst = parseFloat(this.gstDetailsForm.get('IGST')?.value || "0");
+    const utgst = parseFloat(this.gstDetailsForm.get('UGST')?.value || "0");
+    const claimedAmount = parseFloat(this.amountControl?.value || "0");
+    const gstBreakupAmount = sgst + cgst + igst + utgst;
+
+    if (gstBreakupAmount > 0 && gstBreakupAmount <= gstNum && gstNum > 0) {
+      // Get all GST values
+      this.gstDetails.data?.forEach((gst: any) => {
+        gstAmount = gstAmount + gst.Amount
+      })
+      if (claimedAmount > 0) {
+        if (gstNum > 0) {
+          if (gstAmount + gstNum > claimedAmount) {
+            this.snackbarService.error("The GST amount should not be greater than the claimed amount.");
+          } else {
+            isValidGst = true;
+          }
+        } else {
+          this.snackbarService.error("Please enter a valid GST amount.");
+        }
+      } else {
+        this.snackbarService.error("Please enter a claimed amount.");
+      }
+    } else {
+      this.snackbarService.error("Please enter a valid GST amount.");
+    }
+    return isValidGst;
+  }
+
   addGstRow(): void {
-    this.gstDetails.IsBillRaisedInCompanyGST = this.companyGSTForm.value.IsBillRaisedInCompanyGST;
-    this.gstDetails.data.push(this.gstDetailsForm.value);
-    this.control.setValue(this.gstDetails);
-    this.gstDetailsForm.reset();
+    if (this.validateGSTWithClaimed()) {
+      this.gstDetails.IsBillRaisedInCompanyGST = this.companyGSTForm.value.IsBillRaisedInCompanyGST;
+      this.gstDetails.data.push(this.gstDetailsForm.value);
+      this.control.setValue(this.gstDetails);
+      this.gstDetailsForm.reset();
+    }
   }
 
   removeGstRow(index: number): void {
-    if (this.gstDetails.length > 1) {
-      this.gstDetails.splice(index, 1);
+    if (this.gstDetails.data.length > 1) {
+      this.gstDetails.data.splice(index, 1);
     }
   }
 }
