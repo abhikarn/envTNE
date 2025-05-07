b4ecc488827862ac8becb5bcf4c6ee2c967f80e1
Pradip Patoliya <pradip.patoliya@enaviya.com>
2025-05-07 14:58:11 +0530

    dev-07-05

--- src/app/shared/component/material-table/material-table.component.ts
+++ src/app/shared/component/material-table/material-table.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { AttachmentModalComponent } from '../attachment-modal/attachment-modal.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { FormsModule } from '@angular/forms';
+import { AddGstComponent } from '../../dynamic-form/form-controls/gst/add-gst/add-gst.component';
 
 @Component({
   selector: 'app-material-table',
@@ -13,17 +15,24 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     CommonModule,
     MatTableModule,
     MatIconModule,
-    MatTooltipModule
+    MatTooltipModule,
+    FormsModule,
+    AddGstComponent
   ],
   templateUrl: './material-table.component.html',
   styleUrl: './material-table.component.scss'
 })
 export class MaterialTableComponent implements OnChanges {
+  @Input() categoryName: string = '';
   @Input() data: any[] = [];
   @Input() columnConfig: any[] = [];
   @Input() slNoLabel: string = 'Sl. No.';
   @Input() nestedTables: any[] = [];
   @Input() otherFields: any[] = [];
+  @Input() mode: 'preview' | 'approval' | 'finance-approval' = 'preview';
+  @Input() categoryGST: any;
+  @Output() selectionChanged = new EventEmitter<any>();
+  selectAll: boolean = false;
 
   expandedRow: any | null = null;
   processedData: any[] = [];
@@ -35,18 +44,25 @@ export class MaterialTableComponent implements OnChanges {
     if (this.data?.length > 0) {
       this.processedData = this.data.map((row, index) => ({
         ...row,
-        slNo: index + 1
+        slNo: index + 1,
+        selected: true,
+        originalApproved: row.ApprovedAmount || 0,
+        ApprovedAmount: row.ApprovedAmount || 0,
+        remarks: row.remarks || ''
       }));
+
     } else {
       this.processedData = [];
     }
   }
 
   ngOnInit() {
+    console.log(this.categoryGST)
+    this.selectAll = this.processedData.every(r => r.selected);
   }
 
   get visibleColumns() {
-    return this.columnConfig?.filter((c: any) => c.visible && !!c.name);
+    return (this.columnConfig || []).filter((c: any) => c.visible && !!c.name);
   }
 
   get columnKeys() {
@@ -54,7 +70,8 @@ export class MaterialTableComponent implements OnChanges {
   }
 
   get displayedColumns(): string[] {
-    return ['slNo', ...this.columnKeys, 'expand'];
+    const base = (this.mode === 'approval' || this.mode === 'finance-approval') ? ['select'] : ['slNo'];
+    return [...base, ...this.columnKeys, 'actions', 'expand'];
   }
 
   toggleRow(row: any) {
@@ -76,6 +93,34 @@ export class MaterialTableComponent implements OnChanges {
       width: '500px',
       data: { attachments },
       panelClass: 'custom-modal-panel'
+    });
+  }
+
+  toggleSelectAll() {
+    this.processedData.forEach(row => {
+      row.selected = this.selectAll;
+      row.ApprovedAmount = this.selectAll ? row.originalApproved : 0;
+    });
+    this.selectionChanged.emit({
+      name: this.categoryName,
+      data: this.processedData
+    });
+  }
+
+  onRowSelectionChange(row: any) {
+    console.log(row)
+    row.ApprovedAmount = row.selected ? row.originalApproved : 0;
+    this.selectAll = this.processedData.every(r => r.selected);
+    this.selectionChanged.emit({
+      name: this.categoryName,
+      data: this.processedData
+    });
+  }
+
+  onApprovedAmountChange(row: any) {
+    this.selectionChanged.emit({
+      name: this.categoryName,
+      data: this.processedData
     });
   }
 
