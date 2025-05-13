ec2ee8a98d9b2bf81ec3d16baac8a96c6808e619
Pradip Patoliya <pradip.patoliya@enaviya.com>
2025-05-13 13:31:35 +0530

    dev-13-05

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 // environment.ts (for development)
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:44301/api',
-    newApiUrl:'https://localhost:7073/api/'
+    apiUrl: 'https://localhost:44301/api/', // Exising API URL
+    newApiUrl:'https://localhost:7073/api/' // New API URL
 };
