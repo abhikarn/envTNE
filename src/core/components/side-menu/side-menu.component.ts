819c818e71ece8ac3882b8dfc552a618baee40bc
Pradip Patoliya <pradip.patoliya@enaviya.com>
2025-06-10 17:22:31 +0530

    dev-10-06

--- src/core/components/side-menu/side-menu.component.ts
+++ src/core/components/side-menu/side-menu.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../app/shared/service/auth.service';
 
 @Component({
   selector: 'app-side-menu',
-  imports: [RouterModule,ReactiveFormsModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.scss'
 })
 export class SideMenuComponent {
+  displayCode:string=''
+  displayName:string=''
+  profileMenuVisible = false;
+
+  constructor(
+    private router: Router,
+    private auth: AuthService
+  ) { }
+
+  toggleProfileMenu(): void {
+    this.profileMenuVisible = !this.profileMenuVisible;
+  }
+
+  Logout(): void {
+    localStorage.removeItem('userData');
+    this.router.navigate(['/account']); // Adjust route as needed
+  }
+
+  ngOnInit(): void {
+    this.displayCode=this.auth.getUserDisplayCode()
+    this.displayName=this.auth.getUserDisplayName()
+  }
 
 }
