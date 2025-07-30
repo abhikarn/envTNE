2258b2540022614cb450514ac7ad3476e4e62390
Pradip Patoliya <pradip.patoliya@enaviya.com>
2025-07-30 11:05:28 +0530

    dev-30-07

--- tne-api/api/expense.service.ts
+++ tne-api/api/expense.service.ts
@@ -484,6 +484,69 @@ export class ExpenseService extends BaseService {
     }
 
     /**
+     * @param frequencyTypeReferenceParamExpenseCategoryId 
+     * @param frequencyTypeReferenceParamFrequencyTypeId 
+     * @param frequencyTypeReferenceParamUserMasterId 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public expenseGetFrequencyTypeReference(frequencyTypeReferenceParamExpenseCategoryId?: number, frequencyTypeReferenceParamFrequencyTypeId?: number, frequencyTypeReferenceParamUserMasterId?: number, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json' | 'text/json' | 'application/xml' | 'text/xml', context?: HttpContext, transferCache?: boolean}): Observable<DataSubmit>;
+    public expenseGetFrequencyTypeReference(frequencyTypeReferenceParamExpenseCategoryId?: number, frequencyTypeReferenceParamFrequencyTypeId?: number, frequencyTypeReferenceParamUserMasterId?: number, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json' | 'text/json' | 'application/xml' | 'text/xml', context?: HttpContext, transferCache?: boolean}): Observable<HttpResponse<DataSubmit>>;
+    public expenseGetFrequencyTypeReference(frequencyTypeReferenceParamExpenseCategoryId?: number, frequencyTypeReferenceParamFrequencyTypeId?: number, frequencyTypeReferenceParamUserMasterId?: number, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json' | 'text/json' | 'application/xml' | 'text/xml', context?: HttpContext, transferCache?: boolean}): Observable<HttpEvent<DataSubmit>>;
+    public expenseGetFrequencyTypeReference(frequencyTypeReferenceParamExpenseCategoryId?: number, frequencyTypeReferenceParamFrequencyTypeId?: number, frequencyTypeReferenceParamUserMasterId?: number, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json' | 'text/json' | 'application/xml' | 'text/xml', context?: HttpContext, transferCache?: boolean}): Observable<any> {
+
+        let localVarQueryParameters = new HttpParams({encoder: this.encoder});
+        localVarQueryParameters = this.addToHttpParams(localVarQueryParameters,
+          <any>frequencyTypeReferenceParamExpenseCategoryId, 'frequencyTypeReferenceParam.expenseCategoryId');
+        localVarQueryParameters = this.addToHttpParams(localVarQueryParameters,
+          <any>frequencyTypeReferenceParamFrequencyTypeId, 'frequencyTypeReferenceParam.frequencyTypeId');
+        localVarQueryParameters = this.addToHttpParams(localVarQueryParameters,
+          <any>frequencyTypeReferenceParamUserMasterId, 'frequencyTypeReferenceParam.userMasterId');
+
+        let localVarHeaders = this.defaultHeaders;
+
+        const localVarHttpHeaderAcceptSelected: string | undefined = options?.httpHeaderAccept ?? this.configuration.selectHeaderAccept([
+            'application/json',
+            'text/json',
+            'application/xml',
+            'text/xml'
+        ]);
+        if (localVarHttpHeaderAcceptSelected !== undefined) {
+            localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
+        }
+
+        const localVarHttpContext: HttpContext = options?.context ?? new HttpContext();
+
+        const localVarTransferCache: boolean = options?.transferCache ?? true;
+
+
+        let responseType_: 'text' | 'json' | 'blob' = 'json';
+        if (localVarHttpHeaderAcceptSelected) {
+            if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
+                responseType_ = 'text';
+            } else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
+                responseType_ = 'json';
+            } else {
+                responseType_ = 'blob';
+            }
+        }
+
+        let localVarPath = `/api/Expense/GetFrequencyTypeReference`;
+        return this.httpClient.request<DataSubmit>('get', `${this.configuration.basePath}${localVarPath}`,
+            {
+                context: localVarHttpContext,
+                params: localVarQueryParameters,
+                responseType: <any>responseType_,
+                withCredentials: this.configuration.withCredentials,
+                headers: localVarHeaders,
+                observe: observe,
+                transferCache: localVarTransferCache,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
      * @param eligibilityParam 
      * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
      * @param reportProgress flag to report request and response progress.
