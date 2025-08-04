e264d6126ac630a82d71e5abdfa769b4a19389ef
Pradip Patoliya <pradip.patoliya@enaviya.com>
2025-08-04 14:03:04 +0530

    dev-04-08

--- src/app/feature/expense/main-expense/main-expense.component.ts
+++ src/app/feature/expense/main-expense/main-expense.component.ts
@@ -1169,7 +1169,7 @@ export class MainExpenseComponent {
         if (!configCategory) return null;
 
         const requiredFields = configCategory.formControls
-          .filter((control: any) => (!control?.displayPage && control.required))
+          .filter((control: any) => ( !control?.international && !control?.displayPage && control.required))
           .map((control: any) => control.name);
 
         const missingInItems = expenseCategory.data
