644a4e1a97d67c81a5c1caa5bf9d28d7b137b316
Pradip Patoliya <pradip.patoliya@enaviya.com>
2025-08-04 21:44:46 +0530

    dev-04-08

--- src/environment.ts
+++ src/environment.ts
@@ -8,11 +8,3 @@ export const environment = {
     ocrApiBaseUrl: 'https://localhost:7173/', // OCR API Base URL
     googleApiKey: 'AIzaSyBnunT8Wy5gF74vbM9n578mEm7pTk-wYrA', // Google API Key
 };
-
-// export const environment = {
-//     production: false,
-//     apiUrl: 'https://enaviya.co.in/spendmantraMobApi/api/', // Exising API URL
-//     newApiUrl:'https://enaviya.co.in/SpendMantraapi/api/', // New API URL
-//     assetsPath:'https://enaviya.co.in/SpendManraAngular' // New API URL
-//     documentBaseUrl: 'https://enaviya.co.in/spendmantra/Document' // Document base URL
-// };
